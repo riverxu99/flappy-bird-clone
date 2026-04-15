9469a5110701da9fff93cc9c525a6c2a205bd1e1
River <ahe794613@gmail.com>
2026-04-15 22:21:57 +0800

    fix: sound files not found after build

--- game/audio.ts
+++ game/audio.ts
@@ -1,10 +1,10 @@
 import { Howl, Howler } from 'howler'
 
-const flap   = new Howl({ src: ['/sound-effects/jump.wav'],       volume: 0.5 })
-const hit    = new Howl({ src: ['/sound-effects/hitHurt.wav'],    volume: 0.6 })
-const dead   = new Howl({ src: ['/sound-effects/lose.wav'],       volume: 0.7 })
-const pickup = new Howl({ src: ['/sound-effects/pickupCoin.wav'], volume: 0.5 })
-const select = new Howl({ src: ['/sound-effects/select.wav'],     volume: 0.4 })
+const flap   = new Howl({ src: [`${import.meta.env.BASE_URL}sound-effects/jump.wav`],       volume: 0.5 })
+const hit    = new Howl({ src: [`${import.meta.env.BASE_URL}sound-effects/hitHurt.wav`],    volume: 0.6 })
+const dead   = new Howl({ src: [`${import.meta.env.BASE_URL}sound-effects/lose.wav`],       volume: 0.7 })
+const pickup = new Howl({ src: [`${import.meta.env.BASE_URL}sound-effects/pickupCoin.wav`], volume: 0.5 })
+const select = new Howl({ src: [`${import.meta.env.BASE_URL}sound-effects/select.wav`],     volume: 0.4 })
 
 export function setMuted(m: boolean): void {
   Howler.mute(m)
